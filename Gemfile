35ee982ec2eba2029c77c3ebefcc9039d19fc48d
Alex Upits <alex.upits@gmail.com>
2016-04-14 17:49:12 +0000

    add style to forms and forms to create an update recipes

--- Gemfile
+++ Gemfile
@@ -3,14 +3,15 @@ source 'https://rubygems.org'
 #ruby '2.1.4'
 gem 'rails', '4.2.5'
 gem 'sass-rails', '~> 5.0'
-gem 'bootstrap-sass', '~> 3.3.6'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
+gem 'therubyracer', platforms: :ruby
+gem 'less-rails'
+gem 'twitter-bootstrap-rails'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
-
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,5 @@
- class RecipesController < ApplicationController
-  
-  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :edit, :update, :destroy]
    
   def index
     @recipes = Recipe.all
@@ -8,10 +7,44 @@
   
   def show
   
- end
+  end
  
- private
-    # Use callbacks to share common setup or constraints between actions.
+  def new
+    @recipe = Recipe.new
+  end
+ 
+  def create 
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(1)
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to @recipe, notice: 'recipe was successfully created.' }
+        format.json { render :show, status: :created, location: @recipe }
+      else
+        format.html { render :new }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update 
+    respond_to do |format|
+      if @recipe.update(recipe_params)
+        format.html { redirect_to @recipe, notice: 'Recipe was successfully updated.' }
+        format.json { render :show, status: :ok, location: @recipe }
+      else
+        format.html { render :edit }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+    
     def set_recipe
       @recipe = Recipe.find(params[:id])
     end
