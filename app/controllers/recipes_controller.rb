43e105800de08a522d23b13bf4997017669d53a6
Alex Upits <alex.upits@gmail.com>
2016-04-14 00:27:22 +0000

    Add show view and implemented show controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,19 @@
  class RecipesController < ApplicationController
+  
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
    
   def index
     @recipes = Recipe.all
   end
   
+  def show
+  
+ end
+ 
+ private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+ 
 end
